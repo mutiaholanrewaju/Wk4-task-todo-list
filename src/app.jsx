927150c3bb45efe0227daff75255168e76f1c5ab
Ruqayat <yinkaajibade97@gmail.com>
2021-06-07 22:56:19 +0100

    final commit

--- src/app.jsx
+++ src/app.jsx
@@ -9,16 +9,18 @@ function App() {
 
 	const [inputText, setInputText] = useState("");
 	const [todos, setTodos] = useState([]);
-	const [todoEditingText, setTodoEditingText] = useState("");
 
   return (
-	<div className="App" >
-		<h1>My Todo List</h1>
-		<Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos}  />
-		<TodoList todos={todos} setTodos={setTodos}
-		 todoEditingText={todoEditingText} setTodoEditingText={setTodoEditingText} />
-		
-	</div>
+    <div className="App">
+      <h1>My Todo List</h1>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <TodoList todos={todos} setTodos={setTodos} setInputText={setInputText} />
+    </div>
   );
 };
 
